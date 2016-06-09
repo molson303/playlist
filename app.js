c41940661acec447bdc41296f4f912e5d9febd6a
MIchael <michael.olson756@gmail.com>
2016-06-09 15:00:21 -0600

    added pics to 2nd page. Bin complete. Buttons finished.

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ httpRequest.onreadystatechange = function(){
          if(httpRequest.status < 400) {
            var object = JSON.parse(httpRequest.responseText);
            for (var i = 0; i < object.results.length; i++) {
+             //was line 18 needed?
              bucket.push(object.results[i].cover_art)
              var one = Math.floor(Math.random()*object.results.length);
              var two = Math.floor(Math.random()*object.results.length);
@@ -21,7 +22,8 @@ httpRequest.onreadystatechange = function(){
              album1.innerHTML = '<img src=images/' + object.results[one].cover_art + '>'
              album2.innerHTML = '<img src=images/' + object.results[two].cover_art + '>'
              album3.innerHTML = '<img src=images/' + object.results[three].cover_art + '>'
-
+             //no container was ever used on this. content was pushed directly back
+             //to the existing node. CHANGE ALL TO CONTAINER TO SEE WHAT HAPPENS
 
 
 
--- page_two.js
+++ page_two.js
@@ -1,43 +1,55 @@
-var a = document.getElementById("pic4")
-var b = document.getElementById("pic5")
-var c = document.getElementById("pic6")
-var d = document.getElementById("pic7")
-var e = document.getElementById("pic8")
-var button = document.getElementById('buttoncolor2')
 
-httpRequest = new XMLHttpRequest();
+var clear = document.getElementById('buttoncolor1')
+var submit = document.getElementById('buttoncolor2')
 var container = document.getElementById("pic4")
+var bucket = document.getElementById('textboxarea')
+httpRequest = new XMLHttpRequest();
 
 document.addEventListener('DOMContentLoaded', function() {
     event.preventDefault();
   httpRequest.onreadystatechange = function(){
 if (httpRequest.readyState === 4) {
   if(httpRequest.status < 400) {
-    var objectOne = JSON.parse(httpRequest.responseText);
-      for (var i = 0; i < objectOne.results.length; i++) {
-        var oneA = objectOne.results[0].cover_art;
-          a.innerHTML = '<img src=images/' + oneA + '>'
-        var twoB = objectOne.results[1].cover_art;
-          b.innerHTML = '<img src=images/' + twoB + '>'
-        var threeC = objectOne.results[2].cover_art;
-          c.innerHTML = '<img src=images/' + threeC + '>'
-        var fourD = objectOne.results[3].cover_art;
-          d.innerHTML = '<img src=images/' + fourD + '>'
-        var fiveE = objectOne.results[4].cover_art;
-          e.innerHTML = '<img src=images/' + fiveE + '>'
+    var objectOne = JSON.parse(httpRequest.responseText).results;
 
+function clicker(pics){
+  pics.addEventListener('click', function(){
+  var newNode = document.createElement('div')
+  bucket.appendChild(newNode)
+  newNode.innerHTML = pics.id
+  })
+}
 
-
-      
-
-          }
+objectOne.forEach(function(x,i){
+var choices = document.createElement('img');
+choices.classList.add('coverPics');
+choices.id = objectOne[i].artist + ": " + objectOne[i].title
+container.appendChild(choices);
+choices.src = "images/" + objectOne[i].cover_art;
+clicker(choices);
+          })
         }
       }
-
     }
-
-
 httpRequest.open('GET', 'https://lit-fortress-6467.herokuapp.com/object');
 httpRequest.send();
+})
 
+clear.addEventListener("click", function(){
+bucket.innerHTML = "";
+})
+
+submit.addEventListener("click", function(){
+  var postRequest = new XMLHttpRequest();
+  postRequest.onreadystatechange = function(){
+    if (postRequest.readyState === 4) {
+      if(postRequest.status < 400) {
+      }
+    }
+    var postelement = (postRequest.responseText)
+    bucket.innerHTML = "You just made a " + postelement;
+
+  }
+  postRequest.open('POST',  'https://lit-fortress-6467.herokuapp.com/post', true);
+  postRequest.send();
 })
